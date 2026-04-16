7cb1b329d10dd3a9d0bd88584e1c6ed75a73493b
hnuke <hstuquifatec@gmail.com>
2026-04-16 18:31:41 -0300

    wip: menu hamburguer

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,14 +1,17 @@
 const hamburguer = document.querySelector('.hamburguer');
 const navLinks = document.querySelector('.nav-links');
+const nav = document.querySelector('nav');
 let menuOpen = false;
 
 hamburguer.addEventListener('click', () => {
     if (menuOpen == false) {
-        navLinks.style.display = "block";
+        navLinks.style.display = "flex";
+        nav.style.display = "flex"
         menuOpen = true;
     }
     else if (menuOpen == true){
         navLinks.style.display = "none";
+        nav.style.display = "none";
         menuOpen = false;
     }
 });
